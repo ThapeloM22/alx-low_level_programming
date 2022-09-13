4799a6ff50d1fbb93f531263004ea2edd8a3f59b
ThapeloM22 <thapelo.mashaba@gmail.com>
2022-09-13 12:54:00 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints _Puchar
+ *
+ * Description: Prints _Puchar
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
+
+
